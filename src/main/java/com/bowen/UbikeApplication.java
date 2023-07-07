cc7fd7a4496a701f3bff5549bc163ec1b5463c52
bowen528 <hhilary5432@gmail.com>
2023-07-08 02:46:43 +0800

    「 初始化project 」

---
+++ src/main/java/com/bowen/UbikeApplication.java
@@ -0,0 +1,13 @@
+package com.bowen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bowen/UbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
